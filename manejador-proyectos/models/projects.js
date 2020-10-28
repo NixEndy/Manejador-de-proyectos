dad7d8f536be4b6c931eb3ae28f4a57e4e11df83
NixEndy <olague798317@gmail.com>
2020-10-28 00:08:54 -0700

    Avance de modelo project

--- manejador-proyectos/models/projects.js
+++ manejador-proyectos/models/projects.js
@@ -11,3 +11,73 @@ const schema = mongoose.Schema({
   _productOwner: String,
   _members: [Member]
 });
+
+class Project{
+  class(name,requestedDate,startDate,description,projectManager,productOwner,members){
+    this._name = name,
+    this._requestedDate = requestedDate,
+    this._startDate= startDate,
+    this._description = description,
+    this._projectManager = projectManager,
+    this._members = members
+  }
+  get name(){
+    return this.name;
+  }
+
+  set name(v){
+    this.name = v;
+  }
+  //
+  get requestedDate(){
+    return this.requestedDate;
+  }
+
+  set requestedDate(v){
+    this.requestedDate = v;
+  }
+  //
+  get startDate(){
+    return this.startDate;
+  }
+
+  set startDate(v){
+    this.startDate = v;
+  }
+  //
+  get description(){
+    return this.description;
+  }
+
+  set description(v){
+    this.description = v;
+  }
+  //
+  get projectManager(){
+    return this.projectManager;
+  }
+
+  set projectManager(v){
+    this.projectManager = v;
+  }
+  //
+  get productOwner(){
+    return this.productOwner;
+  }
+
+  set productOwner(v){
+    this.productOwner = v;
+  }
+  //
+  get members(){
+    return this.members;
+  }
+
+  set members(v){
+    this.members = v;
+  }
+}
+
+schema.plugin(mongoosePaginate);
+schema.loadClass(Project);
+module.exports = mongoose.model('Project', schema);
