d936793e13b67d944d4070f1b7a81d5c24cdeafc
NixEndy <olague798317@gmail.com>
2020-10-28 00:29:51 -0700

    Avance controller stories y routes

---
+++ manejador-proyectos/controllers/stories.js
@@ -0,0 +1,19 @@
+const async = require('async');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+//const config = require('config');
+const Poject = require('../models/project');
+
+//const jwtKey = config.get('secret.key');
+
+function moveProductBacklog(req, res) {
+
+}
+
+function moveReleaseBacklog(req, res) {
+
+}
+
+function moveSprintBacklog(req, res) {
+
+}
--- manejador-proyectos/routes/projects.js
+++ manejador-proyectos/routes/projects.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var projectCtrl = require('../controllers/project');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
