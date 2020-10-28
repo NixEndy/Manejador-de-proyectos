61d20d43e0b500cea10939253fdb18998fe5bf01
NixEndy <olague798317@gmail.com>
2020-10-27 23:54:10 -0700

    Avance de modelos skills, stories

--- manejador-proyectos/models/skill.js
+++ manejador-proyectos/models/skill.js
@@ -7,3 +7,30 @@ const schema = mongoose.Schema({
   _rank: String
 });
 
+class Skills{
+  constructor(name,rank){
+    this._name = name,
+    this._rank = rank
+  }
+
+  get name(){
+    return this.name;
+  }
+
+  set name(v){
+    this.name = v;
+  }
+  //
+  get rank(){
+    return this.rank;
+  }
+
+  set rank(v){
+    this.rank = v;
+  }
+
+}
+
+schema.plugin(mongoosePaginate);
+schema.loadClass(Skills);
+module.exports = mongoose.model('Skills', schema);
--- manejador-proyectos/models/stories.js
+++ manejador-proyectos/models/stories.js
@@ -3,14 +3,73 @@ const mongoosePaginate = require('mongoose-paginate-v2');
 
 
 const schema = mongoose.Schema({
-  _name: String,
-  _priority: String,
-  _size: Int16Array,
-  _role: String,
-  _functionality: String,
-  _benefit: String,
-  _acceptanceName: String,
-  _context: String,
-  _event: String,
-  _result: String
+  _como: String,
+  _quiero: String,
+  _manera: Sting,
+  _dado: String,
+  _cuando: String,
+  _entonces: String,
 });
+
+class Stories{
+  constructor (como,quiero,manera,dado,cuando,entonces){
+    this._como = como,
+    this._quiero = quiero,
+    this._manera = manera,
+    this._dado = dado,
+    this._cuando = cuando,
+    this._entonces = entonces
+  }
+
+    get como(){
+      return this.como;
+    }
+
+    set como(v){
+      this.como = v;
+    }
+    //
+    get quiero(){
+      return this.quiero;
+    }
+
+    set quiero(v){
+      this.quiero = v;
+    }
+    //
+    get manera(){
+      return this.manera;
+    }
+
+    set manera(v){
+      this.manera = v;
+    }
+    //
+    get dado(){
+      return this.dado;
+    }
+
+    set dado(v){
+      this.dado = v;
+    }
+    //
+    get cuando(){
+      return this.cuando;
+    }
+
+    set cuando(v){
+      this.cuando = v;
+    }
+    //
+    get entonces(){
+      return this.entonces;
+    }
+
+    set entonces(v){
+      this.entonces = v;
+    }
+}
+
+schema.plugin(mongoosePaginate);
+schema.loadClass(Stories);
+module.exports = mongoose.model('Stories', schema);
