6a7cab61c43a585b7ffe6918666f300476327476
Marcela Barrera <a311005@uach.mx>
2020-11-29 19:33:59 -0700

    Added angular, component and routing module

---
+++ manejador-proyectos/my-app/src/app/components/story-card/story-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-story-card',
+  templateUrl: './story-card.component.html',
+  styleUrls: []
+})
+export class StoryCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
