cc57991385ad405f2fcf473c33bed476a541aeef
Marcela Barrera <a311005@uach.mx>
2020-11-29 21:24:09 -0700

    Bootstrap

--- manejador-proyectos/my-app/src/app/app.module.ts
+++ manejador-proyectos/my-app/src/app/app.module.ts
@@ -2,13 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { StoryCardComponent } from './story-card/story-card.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    StoryCardComponent
+    AppComponent
+    
   ],
   imports: [
     BrowserModule,
